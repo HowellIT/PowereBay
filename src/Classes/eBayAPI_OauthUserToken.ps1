35428517c162e02aba47797dad7422165e34c1c2
Anthony Howell <anthony@howell-it.com>
2018-11-14 11:49:33 -0800

    Now accepts a hashtable in the constructor

--- src/Classes/eBayAPI_OauthUserToken.ps1
+++ src/Classes/eBayAPI_OauthUserToken.ps1
@@ -16,6 +16,24 @@ Class eBayAPI_OauthUserToken {
         $this.RefreshTokenExpires = (Get-Date).AddSeconds($refresh_token_expires_in)
     }
 
+    eBayAPI_OauthUserToken(
+        [hashtable]$In
+    ){
+        If(
+            $In.ContainsKey('access_token') -and
+            $In.ContainsKey('expires_in') -and
+            $In.ContainsKey('refresh_token') -and
+            $In.ContainsKey('refresh_token_expires_in')
+        ){
+            $this.Expires = (Get-Date).AddSeconds($In['expires_in'])
+            $this.Token = $In['access_token']
+            $this.RefreshToken = $In['refresh_token']
+            $this.RefreshTokenExpires = (Get-Date).AddSeconds($In['refresh_token_expires_in'])
+        }Else{
+            Throw 'Improperly formatted hash table'
+        }
+    }
+
     [string]ToString(){
         return $this.Token
     }
