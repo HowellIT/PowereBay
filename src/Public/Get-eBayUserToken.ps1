aa2b183353023ce1e89dfc9ca8704aa0f5e1fa44
Anthony Howell <anthony@howell-it.com>
2018-11-14 06:50:06 -0800

    fixed the encoding, it works. still rough though

--- src/Public/Get-eBayUserToken.ps1
+++ src/Public/Get-eBayUserToken.ps1
@@ -8,11 +8,11 @@ Function Get-eBayUserToken {
     )
     $baseUri = 'https://api.ebay.com/identity/v1/oauth2/token/'
     # Doc on auth format: https://developer.ebay.com/api-docs/static/oauth-base64-credentials.html
-    $encodedAuthorization = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$ClientID`:$ClientSecret")) #this is broken
+    $encodedAuthorization = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$ClientID`:$ClientSecret")) #this is broken
     Write-Verbose $encodedAuthorization
     $headers = @{
         'Content-Type' = 'application/x-www-form-urlencoded'
-        Authorization = "Basic SG93ZWxsSVQtRVNTSG93ZWwtUFJELTAyY2NiZGVlZS1mZWQwZjg4MzpQUkQtMmNjYmRlZWVmMjc5LTQ4MjgtNDAxZC1iNDQwLTkzNTU="
+        Authorization = "Basic $encodedAuthorization"
     }
     Write-Verbose $headers.ToString()
 
@@ -24,14 +24,9 @@ Function Get-eBayUserToken {
         "&code=$encodedAuthCode"
         "&redirect_uri=$encodedRUName"
     ) -join ''
-    <#$body = @(
-        "grant_type=authorization_code"
-        "&code=$AuthorizationCode"
-        "&redirect_uri=$RuName"
-    )#>
-    #$body = "grant_type=authorization_code&code=$([System.Web.HttpUtility]::ParseQueryString($AuthorizationCode))&redirect_uri=$([System.Web.HttpUtility]::ParseQueryString($RuName))"
     Write-Verbose $body.ToString()
 
     #Invoke-RestMethod -Uri $baseUri -Body $body -Headers $headers -Method Post
-    Invoke-WebRequest -Uri $baseUri -Body $body -Headers $headers -Method Post
+    $response = Invoke-WebRequest -Uri $baseUri -Body $body -Headers $headers -Method Post
+    $response.Content | ConvertFrom-Json
 }
