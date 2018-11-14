0a68754e905f23ec094b5f1aefc5227b83b0f0ae
Anthony Howell <anthony@howell-it.com>
2018-11-14 11:18:22 -0800

    Added comments, made parameters mandatory

--- src/Public/Get-eBayAuthenticationCode.ps1
+++ src/Public/Get-eBayAuthenticationCode.ps1
@@ -2,44 +2,68 @@ Add-Type -AssemblyName System.Web
 Function Get-eBayAuthenticationCode {
     [cmdletbinding()]
     Param(
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$ClientID,
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$RUName
     )
     <#
         Doc on flow: https://developer.ebay.com/api-docs/static/oauth-authorization-code-grant.html
     #>
-    #$encodedAuthorization = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$ClientID`:$ClientSecret"))
+    # URL encode our parameters
     $encodedClientID = [System.Web.HttpUtility]::UrlEncode($ClientID)
     $encodedRUName = [System.Web.HttpUtility]::UrlEncode($RUName)
+
+    # Other variables
     $baseUri = 'https://auth.ebay.com/oauth2/authorize'
     $scope = 'https://api.ebay.com/oauth/api_scope/sell.inventory%20https://api.ebay.com/oauth/api_scope/sell.fulfillment'
+
+    # Build the logon uri
     $logonUri = "$baseUri`?client_id=$encodedClientID&redirect_uri=$encodedRUName&response_type=code&scope=$scope&prompt=login"
     Write-Verbose "Logon URL: $logonUri"
+
+    # Build a form to use with our web object
     $Form = New-Object -TypeName 'System.Windows.Forms.Form' -Property @{
         Width = 680
         Height = 640
     }
+
+    # Build the web object to brows to the logon uri
     $Web = New-Object -TypeName 'System.Windows.Forms.WebBrowser' -Property @{
         Width = 680
         Height = 640
         Url = $logonUri
     }
+
+    # Add the document completed script to detect when the code is in the uri
     $DocumentCompleted_Script = {
         if ($web.Url.AbsoluteUri -match "error=[^&]*|code=[^&]*") {
             $form.Close()
         }
     }
+
+    # Add controls to the form
     $web.ScriptErrorsSuppressed = $true
     $web.Add_DocumentCompleted($DocumentCompleted_Script)
     $form.Controls.Add($web)
     $form.Add_Shown({ $form.Activate() })
+
+    # Run the form
     [void]$form.ShowDialog()
 
+    # Parse the output
     $QueryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
     $Response = @{ }
     foreach ($key in $queryOutput.Keys) {
         $Response["$key"] = $QueryOutput[$key]
     }
+
+    # Return the output
+    # eventually this will be something more secure than just outputting it... I hope.
     If($Response['isAuthSuccessful']){
         $Response['code']
     }Else{
--- src/Public/Get-eBayUserToken.ps1
+++ src/Public/Get-eBayUserToken.ps1
@@ -1,24 +1,43 @@
 Function Get-eBayUserToken {
     [cmdletbinding()]
     Param(
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$ClientID,
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$ClientSecret,
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$RuName,
+        [Parameter(
+            Mandatory = $true
+        )]
         [string]$AuthorizationCode
     )
+    # Base uri for getting user tokens
     $baseUri = 'https://api.ebay.com/identity/v1/oauth2/token/'
+
+    # Format for authorization header
     # Doc on auth format: https://developer.ebay.com/api-docs/static/oauth-base64-credentials.html
     $encodedAuthorization = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$ClientID`:$ClientSecret"))
     Write-Verbose $encodedAuthorization
+
+    # Build the headers
     $headers = @{
         'Content-Type' = 'application/x-www-form-urlencoded'
         Authorization = "Basic $encodedAuthorization"
     }
     Write-Verbose $headers.ToString()
 
+    # URL encode the parameters
     $encodedAuthCode = [System.Web.HttpUtility]::UrlEncode($AuthorizationCode)
     $encodedRUName = [System.Web.HttpUtility]::UrlEncode($RuName)
 
+    # Build the body using the URL encoded parameters
     $body = @(
         "grant_type=authorization_code"
         "&code=$encodedAuthCode"
@@ -26,6 +45,9 @@ Function Get-eBayUserToken {
     ) -join ''
     Write-Verbose $body.ToString()
 
+    # Send the request
     $response = Invoke-WebRequest -Uri $baseUri -Body $body -Headers $headers -Method Post
+
+    # Return the response
     $response.Content | ConvertFrom-Json
 }
