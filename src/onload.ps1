adc55f98aba2b92b01a5a83f1106db14557e16c2
Anthony Howell <anthony@howell-it.com>
2020-11-02 19:49:17 -0800

    fixed module parts

--- src/onload.ps1
+++ src/onload.ps1
@@ -2,5 +2,5 @@
     Code in this file will be added to the end of the .psm1. For example,
     you should set variables or other environment settings here.
 #>
-Get-ZCrmConfig -Silent
+Get-PeBayConfig -Silent
 $script:configStack = [System.Collections.Stack]::new()
--- src/using.ps1
+++ src/using.ps1
@@ -2,5 +2,3 @@
     Code in this file will be added to the beginning of the .psm1. For example,
     you should place any using statements here.
 #>
-Get-PeBayConfig -Silent
-$script:configStack = [System.Collections.Stack]::new()
